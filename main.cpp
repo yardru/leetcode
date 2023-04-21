a264c941833062d3cc05a95c1c715ab2474e9085
yardru <yardru@yandex.ru>
2023-04-21 08:19:38 +0300

    solution template added

--- main.cpp
+++ main.cpp
@@ -3,6 +3,18 @@
 
 using namespace std;
 
+class Solution {
+public:
+    void run(void) {
+        struct {
+        } tests[] = {
+            {}
+        };
+        for (auto & test : tests)
+            cout << "!!!\n";
+    }
+};
+
 int main(void) {
     return 0;
 }
